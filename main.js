b76bd2bf9d88facf6a5982089f7d858346902b3c
Julio Tsutsui <julio.tsutsui@gmail.com>
2019-04-04 09:17:07 -0300

    Função de exibição funcionando

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 const app = require('./config/server');
 const db = require('./config/firestore');
-
+const tempRef = db.collection("temp").orderBy("data","desc").orderBy("hora","desc").limit(4);
 app.listen("3000", ()=> {
     console.log('Servidor está ON'); 
 });
 
 app.get("/api",(req,res)=>{
     var arr = [];
-    db.collection("users").get().then((querySnapshot) => {
+    tempRef.get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             var temp = new Object();
             temp["_id"] = doc.id;
-            temp["dia"] = doc.data().dia;
-            temp["temperatura"] = doc.data().temp;
+            temp["data"] = doc.data().data;
+            temp["temperatura"] = doc.data().temperatura;
             temp["hora"] = doc.data().hora;
             arr.push(temp);
+
         });
         res.json(arr);
     });
@@ -23,7 +24,7 @@ app.get("/api",(req,res)=>{
 app.post("/api",(req,res)=>{
     var dados = req.body;
 
-    db.collection("users").add(dados)
+    db.collection("temp").add(dados)
     .then(function (docRef) {
         console.log("Documento criado com o ID: ", docRef.id);
         res.json({ok:1});
